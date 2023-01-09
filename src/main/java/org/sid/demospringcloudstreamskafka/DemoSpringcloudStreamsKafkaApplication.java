81af56cfbba39c669f2997924e86e1c4a6e9d650
NamasseZineb <z.namasse@gmail.com>
2023-01-09 17:15:51 -0500

    "Kafka2"

--- src/main/java/org/sid/demospringcloudstreamskafka/DemoSpringcloudStreamsKafkaApplication.java
+++ src/main/java/org/sid/demospringcloudstreamskafka/DemoSpringcloudStreamsKafkaApplication.java
@@ -2,6 +2,16 @@ package org.sid.demospringcloudstreamskafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.sid.demospringcloudstreamskafka.entities.PageEvent;
+
+
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.*;
+import org.apache.kafka.streams.kstream.*;
+import org.springframework.boot.CommandLineRunner;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class DemoSpringcloudStreamsKafkaApplication {
